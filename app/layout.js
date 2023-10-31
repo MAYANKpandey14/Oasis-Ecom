9ad1dae82912dc350cef18beeed19c20e2ed6eb3
MAYANKpandey14 <mayankpandeyofficial404@gmail.com>
2023-10-31 23:56:43 +0530

    Added toploader animation

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,33 @@
+import { GlobalProvider } from "./GlobalProviders";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import NextTopLoader from "nextjs-toploader";
 
-
-import { GlobalProvider } from './GlobalProviders'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Oasis',
-  description: 'E-commerce application',
-}
+  title: "Oasis",
+  description: "E-commerce application",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <GlobalProvider>
-        <Navbar/>
-        {children}
-        <Footer/>
+          <Navbar />
+          <NextTopLoader
+            color="#000000"
+            height={4}
+            showSpinner={true}
+            easing="ease"
+          />
+          {children}
+          <Footer />
         </GlobalProvider>
       </body>
     </html>
-  )
+  );
 }
